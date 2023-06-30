c3329cf59968723ff7a53e16fa1a9aa84b161f4d
1san4ik <1san4ik@gmail.com>
2023-06-30 03:20:40 +0300

    Homework 4

---
+++ index.js
@@ -0,0 +1,9 @@
+const num1 = prompt('Введіть перше число')
+const num2 = prompt('Введіть друге число')
+
+alert('Користувач ввів: ' + num1 + ' і ' + num2 + '\n' +
+  num1 + ' + ' + num2 + ' = ' + (num1 + num2) + '\n' +
+  num1 + ' - ' + num2 + ' = ' + (num1 - num2) + '\n' +
+  num1 + ' * ' + num2 + ' = ' + (num1 * num2) + '\n' +
+  num1 + ' / ' + num2 + ' = ' + (num1 / num2)
+)
